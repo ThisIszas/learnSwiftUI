17c4dca49585882ab013cd05763042dfb7c07828
zhengli <li.zheng@capgemini.com>
2022-06-12 16:44:43 +0800

    init

---
+++ BuildingListsAndNavigation/Complete/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,54 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing the details for a landmark.
+*/
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    /// 因为不需要动态更新, 所以不用@State
+    var landmark: Landmark
+
+    var body: some View {
+        /// VStack不能滑动
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
---
+++ BuildingListsAndNavigation/Complete/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,42 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing a list of landmarks.
+*/
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+/*  隐藏自带箭头方式1
+                LandmarkRow(landmark: landmark).background(
+                    NavigationLink("", destination: {
+                        LandmarkDetail(landmark: landmark)
+                    }).opacity(0)
+                );
+*/
+                /// 隐藏自带箭头方式1
+                ZStack{
+                    LandmarkRow(landmark: landmark);
+                    NavigationLink("", destination: {
+                        LandmarkDetail(landmark: landmark)
+                    }).opacity(0)
+                }
+            }.listStyle(.plain)  /// 使cell full width
+            .navigationTitle("xxx")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
